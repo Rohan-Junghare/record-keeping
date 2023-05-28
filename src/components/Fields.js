23120e62d4e476fd16f0ff9809c5f11ef1766991
Rohan-Junghare <webmaster240005@gmail.com>
2023-05-28 17:46:55 +0530

    update

--- src/components/Fields.js
+++ src/components/Fields.js
@@ -7,9 +7,10 @@ const Fields = ({name, email, index}) => {
     <div className="data_val">
           <h4>{name}</h4>
           <h4>{email}</h4>
-          <h4 className='remove'><Button oncvariant="contained" color="error">
-           <ClearIcon />
-          </Button></h4>
+          <h4 className='remove'>
+            <Button oncvariant="contained" color="error">
+              <ClearIcon />
+            </Button></h4>
         </div>
   )
 }
